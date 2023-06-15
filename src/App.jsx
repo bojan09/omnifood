510df743e91303508582dd690316cdd536eb13c5
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-15 15:41:31 +0200

    initial folder structure and setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import React from "react";
 // components
 import { Navbar, Footer } from "./components";
 
+// pages
+import { Home, Sponsors, HowItWorks, About, Pricing, Contact } from "./pages";
+
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Home />
+      <Sponsors />
+      <HowItWorks />
+      <About />
+      <Pricing />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as Sponsors } from "./Sponsors";
+export { default as HowItWorks } from "./HowItWorks";
+export { default as About } from "./About";
+export { default as Pricing } from "./Pricing";
+export { default as Contact } from "./Contact";
