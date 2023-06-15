e9c93f5523765897902f48858e69c4b5f0634f1b
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-15 15:31:40 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// components
+import { Navbar, Footer } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rufina: ["Rufina", "serif"],
+        oxygen: ["Oxygen", "sans-serif"],
+      },
+      colors: {
+        primaryColor: "#e67e22",
+        secondaryColor: "#eb984e",
+        primaryBackground: "#fdf2e9",
+        secondaryBackground: "#fae5d3",
+        darkColor: "#45260a",
+        shadeColor: "#cf711f",
+        greenDarkColor: "#51cf66",
+        greenLightColor: "#94d82d",
+      },
+      screens: {
+        xs: "350px",
+      },
+    },
+  },
+  plugins: [],
+};
