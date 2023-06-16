d7a3c39d7274af35f37dc716828d1db6aadf6480
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-16 17:43:15 +0200

    About mobile - complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,47 +18,57 @@ const About = () => {
       {/* Main - Container */}
       <div className="flex xs:flex-col justify-center items-center mt-[3rem]">
         {/* Container -- 1 */}
-        <div className="w-[300px] mb-[3.5rem]">
+        <div className="w-[300px] mb-[3.5rem] shadow-md rounded-lg pb-4">
           {/* Meal - Image */}
-          <img src={vegeterian_meal} alt="vegeterian_meal" />
-          <div>
+          <img
+            src={vegeterian_meal}
+            alt="vegeterian_meal"
+            className="rounded-t-lg"
+          />
+          <div className="p-4">
             {/* Meal - Category */}
-            <span>vegeterian</span>
+            <span className="bg-greenDarkColor py-[4px] px-[8px] rounded-xl uppercase text-[12px] text-white font-medium">
+              vegeterian
+            </span>
 
             {/* Meal - Heading */}
-            <h2>Japanese Gyozas</h2>
+            <h2 className="text-xl font-semibold my-4">Japanese Gyozas</h2>
 
             {/* Meal - Description */}
-            <div>
-              <p>650 calories</p>
-              <p>NutriScore ® 74</p>
-              <p>4.9 rating (537)</p>
+            <div className="flex flex-col justify-center gap-4 pl-4">
+              <p className="font-light xs:text-[15px]">650 calories</p>
+              <p className="font-light xs:text-[15px]">NutriScore ® 74</p>
+              <p className="font-light xs:text-[15px]">4.9 rating (537)</p>
             </div>
           </div>
         </div>
 
         {/* Container -- 2 */}
-        <div className="w-[300px]">
+        <div className="w-[300px] mb-[3.5rem] shadow-md rounded-lg pb-4">
           {/* Meal - Image */}
           <img
             src={vegeterian_paleo_meal}
             alt="vegeterian_paleo_meal"
-            className=""
+            className="rounded-t-lg"
           />
           <div>
             {/* Meal - Category */}
-            <div>
-              <span>vegeterian</span>
-              <span>paleo</span>
+            <div className="p-4">
+              <span className="bg-greenLightColor py-[4px] px-[8px] rounded-xl uppercase text-[12px] text-white font-medium">
+                vegeterian
+              </span>
+              <span className="bg-secondaryColor py-[4px] px-[8px] rounded-xl uppercase text-[12px] text-white font-medium">
+                paleo
+              </span>
             </div>
             {/* Meal - Heading */}
-            <h2>Japanese Gyozas</h2>
+            <h2 className="text-xl font-semibold my-4 px-4">Avocado Salad</h2>
 
             {/* Meal - Description */}
-            <div>
-              <p>650 calories</p>
-              <p>NutriScore ® 74</p>
-              <p>4.9 rating (537)</p>
+            <div className="flex flex-col justify-center gap-4 pl-6 pb-[1rem]">
+              <p className="font-light xs:text-[15px]">400 calories</p>
+              <p className="font-light xs:text-[15px]">NutriScore ® 92</p>
+              <p className="font-light xs:text-[15px]">4.8 rating (441)</p>
             </div>
           </div>
         </div>
@@ -66,22 +76,27 @@ const About = () => {
         {/* Container -- 3 */}
         <div>
           {/* Heading  */}
-          <h1>Works with any diet:</h1>
+          <h1 className="font-semibold text-xl mb-5">Works with any diet:</h1>
           {/* Other choices  */}
-          <p>Vegetarian</p>
-          <p>Vegan</p>
-          <p>Pescatarian</p>
-          <p>Gluten-free</p>
-          <p>Lactose-free</p>
-          <p>Keto</p>
-          <p>Paleo</p>
-          <p>Low FODMAP</p>
-          <p>Kid-friendly</p>
+          <div className="flex flex-col justify-center items-center gap-4 mb-[4rem]">
+            <p className="font-light xs:text-[15px]">Vegetarian</p>
+            <p className="font-light xs:text-[15px]">Vegan</p>
+            <p className="font-light xs:text-[15px]">Pescatarian</p>
+            <p className="font-light xs:text-[15px]">Gluten-free</p>
+            <p className="font-light xs:text-[15px]">Lactose-free</p>
+            <p className="font-light xs:text-[15px]">Keto</p>
+            <p className="font-light xs:text-[15px]">Paleo</p>
+            <p className="font-light xs:text-[15px]">Low FODMAP</p>
+            <p className="font-light xs:text-[15px]">Kid-friendly</p>
+          </div>
         </div>
       </div>
 
-      <button>
-        <a className="" href="">
+      <button className="flex justify-center items-center mx-auto">
+        <a
+          className="border-b-2 border-primaryColor hover:bg-secondaryColor hover:text-white p-2 transition-all duration-400 ease-in-out"
+          href="#"
+        >
           See all recipes →
         </a>
       </button>
