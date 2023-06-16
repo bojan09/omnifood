4c8b8f3d29606204d0bd5c5f47fe4eb1b0b14c39
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-16 12:32:31 +0200

    Home page, completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,70 +11,72 @@ import deliveredImage6 from "../assets/customers/customer-6.jpg";
 
 const Home = () => {
   return (
-    <div id="#" className="bg-primaryBackground md:flex justify-around ">
-      <div className="flex flex-col justify-center items-center gap-8 pt-5 ">
+    <div
+      id="#"
+      className="bg-primaryBackground md:flex justify-evenly md:px-[20rem] md:pt-[2rem] md:pb-[3rem]"
+    >
+      <div className="md:flex flex-col gap-6 xs:pt-5 md:flex-1">
         <h1 className="xs:text-[35px] md:text-[52px] font-medium text-center px-5 xs:py-4 md:leading-[55px] md:text-start">
           A healthy meal delivered to your door, every single day
         </h1>
-        <p className="px-5 text-center text-[16px]">
+        <p className="px-5 text-center text-[16px] md:text-[18px] md:text-start ">
           The smart 365-days-per-year food subscription that will make you eat
           healthy again. Tailored to your personal tastes and nutritional needs.
         </p>
 
         {/* Buttons */}
-        <div className="flex gap-4 py-4">
-          <button className="bg-primaryColor text-white py-3 px-2 rounded-lg font-medium">
+        <div className="flex gap-4 py-4 md:ml-5">
+          <button className="bg-primaryColor hover:bg-shadeColor transition-all duration-500 ease-in-out text-white py-3 px-2 rounded-lg font-medium">
             <a href="#contact">Start eating well again</a>
           </button>
-          <button className="bg-white py-3 px-2 rounded-lg font-medium">
+          <button className="bg-white  hover:text-primaryColor transition-all duration-500 ease-in-out py-3 px-2 rounded-lg font-medium">
             <a href="#howItWorks">Learn more ↓</a>
           </button>
         </div>
 
         {/* Delivered/Customers Tab  */}
-        <div className="flex justify-evenly px-4 py-4">
+        <div className="flex justify-evenly md:items-center px-4 py-4">
           {/* Customers img tab  */}
           <img
-            className="rounded-full border-white border-[5px] md:w-[55x] md:h-[55px] xs:w-[40px] h-[40px]"
+            className="rounded-full border-white border-[5px] md:w-[60x] md:h-[50px] xs:w-[40px] h-[40px] hover:scale-105 transition-all ease-in-out duration-75"
             src={deliveredImage1}
             alt="customer_img"
           />
           <img
-            className="rounded-full border-white border-[5px] ml-[-1rem] md:w-[55x] md:h-[55px] xs:w-[40px] h-[40px]"
+            className="rounded-full border-white border-[5px] ml-[-1.5rem] md:w-[60x] md:h-[50px] xs:w-[40px] h-[40px] hover:scale-105 transition-all ease-in-out duration-75 "
             src={deliveredImage2}
             alt="customer_img"
           />
           <img
-            className="rounded-full border-white border-[5px] ml-[-1rem] md:w-[55x] md:h-[55px] xs:w-[40px] h-[40px]"
+            className="rounded-full border-white border-[5px] ml-[-1.5rem] md:w-[60x] md:h-[50px] xs:w-[40px] h-[40px] hover:scale-105 transition-all ease-in-out duration-75 "
             src={deliveredImage3}
             alt="customer_img"
           />
           <img
-            className="rounded-full border-white border-[5px] ml-[-1rem] md:w-[55x] md:h-[55px] xs:w-[40px] h-[40px]"
+            className="rounded-full border-white border-[5px] ml-[-1.5rem] md:w-[60x] md:h-[50px] xs:w-[40px] h-[40px] hover:scale-105 transition-all ease-in-out duration-75 "
             src={deliveredImage4}
             alt="customer_img"
           />
           <img
-            className="rounded-full border-white border-[5px] ml-[-1rem] md:w-[55x] md:h-[55px] xs:w-[40px] h-[40px]"
+            className="rounded-full border-white border-[5px] ml-[-1.5rem] md:w-[60x] md:h-[50px] xs:w-[40px] h-[40px] hover:scale-105 transition-all ease-in-out duration-75 "
             src={deliveredImage5}
             alt="customer_img"
           />
           <img
-            className="rounded-full border-white border-[5px] ml-[-1rem] md:w-[55x] md:h-[55px] xs:w-[40px] h-[40px]"
+            className="rounded-full border-white border-[5px] ml-[-1.5rem] md:w-[60x] md:h-[50px] xs:w-[40px] h-[40px] hover:scale-105 transition-all ease-in-out duration-75 "
             src={deliveredImage6}
             alt="customer_img"
           />
-
           <p className="px-5 text-center">
-            <span className="text-primaryColor font-medium">250,000+</span>{" "}
-            meals delivered last year!
+            <span className="text-primaryColor">250,000+</span> meals delivered
+            last year!
           </p>
         </div>
       </div>
 
       {/* Right side - Container */}
-      <div className="px-12 py-[3rem]">
-        <img src={heroImage} alt="hero_img" />
+      <div className="md:flex-1 flex justify-center items-center   px-12 py-[3rem]">
+        <img src={heroImage} alt="hero_img" className="w-[600px]" />
       </div>
     </div>
   );
