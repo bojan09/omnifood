e113b5b2fd6a67fbfb316020b0a77ba8a93693e3
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-17 09:35:01 +0200

    Footer desktop complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ import footerLogo from "../assets/omnifood-logo.png";
 
 const Footer = () => {
   return (
-    <div className="my-[5rem] border-t-2 mx-4">
+    <div className="my-[5rem] border-t-2 xs:w-[90vw] md:w-[80vw] mx-auto md:flex flex-row-reverse justify-center items-center md:gap-[5rem]">
       {/* Top container */}
-      <div className="flex justify-center my-[3rem] gap-5">
+      <div className="flex justify-center my-[3rem] gap-5 md:gap-[5rem]">
         {/* Account - container */}
         <div className="font-light text-sm flex flex-col gap-4">
           <h1 className="mb-5 font-normal">Account</h1>
@@ -36,9 +36,9 @@ const Footer = () => {
       </div>
 
       {/* Bottom container */}
-      <div className="flex gap-8">
+      <div className="flex gap-8 md:gap-[5rem] md:mb-[3rem] ">
         {/* Logo & Copyright - container */}
-        <a href="#" className="w-[50vw]">
+        <a href="#" className="w-[50vw] md:w-[12vw]">
           <img src={footerLogo} alt="logo" className="mb-[4rem]" />
           <p className="font-light text-sm flex flex-col gap-4">
             Copyright © 2023 by Omnifood, Inc. <br />
@@ -50,8 +50,8 @@ const Footer = () => {
           <h1 className="mb-5 font-normal">Contact us</h1>
           <div className="font-light text-sm flex flex-col gap-4">
             <p>
-              623 Harrison St., <br /> 2nd Floor,
-              <br /> San Francisco, <br /> CA 94107
+              623 Harrison St., 2nd Floor <br />
+              San Francisco, CA 94107
             </p>
             <p>
               415-201-6370 <br />
